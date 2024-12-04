07cb848711cb01c5faaab9ca66446f1a4d0491de
Guilherme Silva <guilherme.cedesp01@gmail.com>
2024-12-04 19:20:21 -0300

    Add files via upload

---
+++ assets/js/login.js
@@ -0,0 +1,16 @@
+const showHiddenPassword = () => {
+    const input = document.getElementById('login-pass');
+    const iconEye = document.getElementById('login-eye');
+
+    iconEye.addEventListener('click',()=> {
+        if(input.type === 'password') {
+            input.type = Text
+
+        } else {
+
+
+        }
+    });
+}
+
+showHiddenpassword();
